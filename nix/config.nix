a97bd18c294ad58234415d0881db544f862dc9ae
Thomas Hiscock <thomas.hiscock@wanadoo.fr>
2018-03-18 18:49:30 +0100

    nix: populate default packages in config.nix

--- nix/config.nix
+++ nix/config.nix
@@ -1,21 +1,42 @@
+let
+  # Most common packages for developpement
+  baseDevel = pkgs: with pkgs; [
+    aspell
+    aspellDicts.en
+    aspellDicts.fr
+    cmake
+    gnumake
+    ninja
+    gcc
+    gdb
+    binutils
+    nasm
+    openjdk
+    git
+    gitAndTools.qgit
+    python36
+    python36Packages.pip
+    python36Packages.virtualenv
+    imagemagick
+    nix-prefetch-scripts
+    nix-repl
+  ];
+
+  # X11 packages
+  desktopPkgs = pkgs: with pkgs; [
+    libreoffice
+    vlc
+  ];
+in
 {
-    packageOverrides = pkgs: with pkgs; rec {
-      commonPkgs = buildEnv {
-        name = "commonPkgs";
-        meta = {
-          description = "Common packages for desktop usage";
-        };
-        paths = [
-          cmake
-          libreoffice
-          gcc
-          openjdk
-          python36
-          python36Packages.pip
-          python36Packages.virtualenv
-          nix-prefetch-scripts
-          nix-repl
-        ];
+  packageOverrides = pkgs: with pkgs; rec {
+    commonPkgs = buildEnv {
+      name = "commonPkgs";
+      paths = baseDevel pkgs ++ desktopPkgs pkgs;
+      meta = {
+        description = "Common packages for desktop usage";
       };
     };
+
+  };
 }
